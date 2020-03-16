a9194ea6a1a57893350828e3cf988e4949592c80
Chfaeser <62070587+Chfaeser@users.noreply.github.com>
2020-03-16 17:50:54 +0100

    Layout und Methode für 2.2
    Layout der App erstellt
    Methode für Aufgabe 2.2 implementiert

--- app/src/main/java/com/example/einzelarbeitse2/MainActivity.java
+++ app/src/main/java/com/example/einzelarbeitse2/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.einzelarbeitse2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,51 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    //Methode für Aufgabe 2.2
+
+    public void sortieren (View view) {
+
+        EditText number = findViewById(R.id.editNumber);
+
+        String text = number.getText().toString();
+
+        //Input in ein Array verwandeln
+        int[]array = new int [text.length()];
+        for (int i = 0; i < text.length(); i++) {
+            array[i] = Character.getNumericValue(text.charAt(i));
+        }
+
+        List<Integer> gerade = new ArrayList<>();
+        List<Integer> ungerade = new ArrayList<>();
+
+        //Aufteilen in gerade/ungerade
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0)
+                gerade.add(array[i]);
+            else
+                ungerade.add(array[i]);
+
+        }
+
+        //Sortieren und zusammenfügen der Arraylists
+        Collections.sort(gerade);
+        Collections.sort(ungerade);
+        gerade.addAll(ungerade);
+
+        //Stringbuilder zum umwandeln der Arraylist
+        StringBuilder stringbuilder = new StringBuilder();
+        for (int i = 0; i < gerade.size(); i++) {
+            int a = gerade.get(i);
+            stringbuilder.append(a);
+        }
+
+        String sortiert = stringbuilder.toString();
+
+        TextView ausgabe = findViewById(R.id.textViewResponse);
+        ausgabe.setText(sortiert);
+
+
+
     }
 }
